a3e68514324f80bcd7160726fbbabcc3eda4351a
Hamed Kordlou <hamed@DESKTOP-BP6QIRS>
2022-04-28 16:34:17 +0200

    Renaming predefined classes

--- CheckoutRepository/Calculator.cs
+++ CheckoutRepository/Calculator.cs
@@ -2,7 +2,7 @@
 
 namespace CheckoutRepository
 {
-    public class Class1
+    public class Calculator
     {
     }
 }
---
+++ CheckoutTests/CheckoutTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace CheckoutTests
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void Checkout_without_offer()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
