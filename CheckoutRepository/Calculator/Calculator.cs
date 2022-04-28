972dc32ef6d274abc6830762621c30168faf95af
Hamed Kordlou <hamed@DESKTOP-BP6QIRS>
2022-04-28 19:09:11 +0200

    null args and bad args have been addressed

--- CheckoutRepository/Calculator/Calculator.cs
+++ CheckoutRepository/Calculator/Calculator.cs
@@ -1,6 +1,7 @@
 ﻿using CommonLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CheckoutRepository.Calculator
 {
@@ -8,8 +9,25 @@ namespace CheckoutRepository.Calculator
     {
         public double CalculateCheckout (List<IGroceryItem> items, List<string> shoppingList)
         {
-            //throw new NotImplementedException();
-            return 140;
+            // check if any arg is null
+            if (items == null || shoppingList == null)
+                throw new ArgumentNullException();
+
+            // check if any cost is negative
+            if (items.Any(item => ((GroceryItem)item).Cost < 0))
+                throw new ArgumentException();
+
+            // check if there is an item in shopping list which does not exist in grocery items
+            shoppingList.ForEach(shoppingItem =>
+            {
+                if (!items.Any(item => ((GroceryItem)item).Name == shoppingItem))
+                    throw new ArgumentException();
+            });
+
+
+
+
+            return 0;
         }
     }
 }
